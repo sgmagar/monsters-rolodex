f9a81a1c5fd0560950e2f351a7fe493af4764399
Saurav <sp.gharti@gmail.com>
2020-09-20 10:36:13 +0545

    Updates

---
+++ precache-manifest.b960cf56e29a21a900bca3efe1b3974c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71b8176a5c654255c5b29ef35807e54",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "96ede5344544a3d78597",
+    "url": "/monsters-rolodex/static/css/main.b30da475.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96ede5344544a3d78597",
+    "url": "/monsters-rolodex/static/js/main.fdcea288.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
