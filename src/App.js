e64276c70cd9ab5fb221acbbf0774211cbca44ed
Saurav <sp.gharti@gmail.com>
2020-09-20 10:19:27 +0545

    move search field to single component and filter monsters on search field values

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react"
 import "./App.css"
 import { CardList } from "./components/card-list/card-list.component"
+import { SearchBox } from "./components/search-box/search-box.component"
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       monsters: [],
+      searchField: "",
     }
   }
 
@@ -15,10 +17,21 @@ class App extends Component {
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }))
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    )
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+        <SearchBox placeholder="search monsters" handleChange={this.handleChange} />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     )
   }
